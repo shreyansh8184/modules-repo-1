eaac3fd358e97447a21f4ab4769d05a1b7fcdd4a
erenmetesar <32137957+erenmetesar@users.noreply.github.com>
2019-11-17 05:23:13 +0300

    Update sticklet.py

--- sticklet.py
+++ sticklet.py
@@ -41,11 +41,11 @@ async def sticklet(event):
     image = Image.new("RGBA", (512, 512), (255, 255, 255, 0))
     draw = ImageDraw.Draw(image)
     fontsize = 230
-    font = ImageFont.truetype("RobotoMono-Regular.ttf", size=fontsize)
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", size=fontsize)
 
     while draw.multiline_textsize(sticktext, font=font) > (512, 512):
         fontsize -= 3
-        font = ImageFont.truetype("RobotoMono-Regular.ttf", size=fontsize)
+        font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", size=fontsize)
 
     width, height = draw.multiline_textsize(sticktext, font=font)
     draw.multiline_text(((512-width)/2,(512-height)/2), sticktext, font=font, fill="white")
